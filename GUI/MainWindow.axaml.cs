5a4ad7a9e50f55b4bb87b832116a885ac27057e6
bruno <brunoeckdema@gmail.com>
2023-04-14 23:45:37 -0300

    create project folder structure

---
+++ GUI/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace GUI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
