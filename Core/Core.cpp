3d91292582d0fcda93cd74190ac5d3b2516a330a
bruno <brunoeckdema@gmail.com>
2023-04-16 20:36:25 -0300

    implement simple example (and install oneTBB on my PC)

--- Core/Core.cpp
+++ Core/Core.cpp
@@ -1,12 +1,29 @@
 ﻿// Core.cpp : Defines the entry point for the application.
-//
 
 #include "Core.h"
 
-using namespace std;
+#include <tbb/tbb.h>
 
-int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
+#include <iostream>
+
+int main() {
+    // Input array of numbers
+    int nums[] = { 1, 2, 3, 4, 5 };
+
+    // Number of elements in the array
+    int n = sizeof(nums) / sizeof(nums[0]);
+
+    // Parallel loop using oneTBB
+    tbb::parallel_for(0, n, [&](int i) {
+        nums[i] = nums[i] * nums[i]; // Square each element
+        });
+
+    // Output the squared numbers
+    std::cout << "Squared numbers: ";
+    for (int i = 0; i < n; ++i) {
+        std::cout << nums[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
 }
