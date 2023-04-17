2c3b6cd724491c63bb723fae8e3133b3c177b71c
bruno <brunoeckdema@gmail.com>
2023-04-16 23:14:30 -0300

    implement simple oneTBB example

--- Core/Core.cpp
+++ Core/Core.cpp
@@ -5,25 +5,47 @@
 #include <tbb/tbb.h>
 
 #include <iostream>
+#include <vector>
+#include <chrono>
 
-int main() {
-    // Input array of numbers
-    int nums[] = { 1, 2, 3, 4, 5 };
+int main(int argc, char* argv[]) {
+	// Input array of numbers
+	const auto size = atoi(argv[1]);
+	auto nums = std::vector< int >();
+	for (size_t i = 0; i < size; i++)
+	{
+		nums.push_back(i);
+	}
 
-    // Number of elements in the array
-    int n = sizeof(nums) / sizeof(nums[0]);
+	auto start1 = std::chrono::high_resolution_clock::now();
+	tbb::parallel_for(0, size, [&](int i) {
+		nums[i] = nums[i] * nums[i]; // Square each element
+		});
+	auto stop1 = std::chrono::high_resolution_clock::now();
+	auto duration1 = duration_cast<std::chrono::microseconds>(stop1 - start1);
 
-    // Parallel loop using oneTBB
-    tbb::parallel_for(0, n, [&](int i) {
-        nums[i] = nums[i] * nums[i]; // Square each element
-        });
+	auto start2 = std::chrono::high_resolution_clock::now();
 
-    // Output the squared numbers
-    std::cout << "Squared numbers: ";
-    for (int i = 0; i < n; ++i) {
-        std::cout << nums[i] << " ";
-    }
-    std::cout << std::endl;
+	for (size_t i = 0; i < size; i++)
+	{
+		nums[i] = nums[i] * nums[i]; // Square each element
+	}
+	auto stop2 = std::chrono::high_resolution_clock::now();
+	auto duration2 = duration_cast<std::chrono::microseconds>(stop2 - start2);
 
-    return 0;
+	// Output the squared numbers
+	//std::cout << "Squared numbers: ";
+	//for (int i = 0; i < size; ++i) {
+	//	std::cout << nums[i] << " ";
+	//}
+	//std::cout << std::endl;
+
+	auto duration_time1 = std::chrono::duration_cast<std::chrono::nanoseconds>(stop1 - start1).count();
+	auto duration_time2 = std::chrono::duration_cast<std::chrono::nanoseconds>(stop2 - start2).count();
+	double speedup_factor = static_cast<double>(duration_time2) / duration_time1;
+	std::cout << "Time taken by TBB " << duration1.count() << " microseconds" << std::endl;
+	std::cout << "Time taken by Normal " << duration2.count() << " microseconds" << std::endl;
+	std::cout << "Speedup factor is (greater than 1 indicates that TBB is faster than Normal) " << speedup_factor << std::endl;
+
+	return 0;
 }
